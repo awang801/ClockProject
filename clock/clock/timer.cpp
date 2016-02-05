a379b180074a44ac81b45bd057295e8cf10d623b
Brandon Givens <brandon.a.givens@gmail.com>
2016-02-05 15:59:57 -0600

    Define get and set method definitions

--- clock/clock/timer.cpp
+++ clock/clock/timer.cpp
@@ -4,6 +4,9 @@
 
 timer::timer()
 {
+	m_hours = 0;
+	m_minutes = 0;
+	m_seconds = 0;
 }
 
 timer::~timer()
@@ -17,30 +20,30 @@ void timer::timing()
 
 int timer::getHours()
 {
-
+	return(m_hours);
 }
 
 int timer::getMinutes()
 {
-
+	return(m_minutes);
 }
 
 int timer::getSeconds()
 {
-
+	return(m_seconds);
 }
 
 void timer::setHours(int hours)
 {
-
+	m_hours = hours;
 }
 
 void timer::setMinutes(int minutes)
 {
-
+	m_minutes = minutes;
 }
 
 void timer::setSeconds(int seconds)
 {
-
+	m_seconds = seconds;
 }
