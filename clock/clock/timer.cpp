2b7d1862676e31e289c7f29877a1fec20cf150d3
Brandon Givens <brandon.a.givens@gmail.com>
2016-02-05 15:51:26 -0600

    Create time.h and time.cpp Create basic method definitions

---
+++ clock/clock/timer.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include "timer.h"
+
+
+timer::timer()
+{
+}
+
+timer::~timer()
+{
+}
+
+void timer::timing()
+{
+
+}
+
+int timer::getHours()
+{
+
+}
+
+int timer::getMinutes()
+{
+
+}
+
+int timer::getSeconds()
+{
+
+}
+
+void timer::setHours(int hours)
+{
+
+}
+
+void timer::setMinutes(int minutes)
+{
+
+}
+
+void timer::setSeconds(int seconds)
+{
+
+}
