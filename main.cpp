438db708e3d41c50259b9c73693c2acbb1b1e228
Jballard419 <j.lb4@live.com>
2016-02-09 17:11:47 -0600

    Part of user input done

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <random>
 #include <ctime>
 #include <chrono>
@@ -8,6 +9,73 @@ int main()
 {
 
         return(0);
+}
+/*
+user setting time of clock
+*/
+void Start(timer clk){
+ std::cout << "24 hr mode? (y/n)?" << std::endl;
+ std::string temp;
+ std::cin >> temp;
+ bool inputflag;
+  do{
+
+
+    if(temp=="Y"||temp=="y")
+    {
+      /*set24flag*/
+      inputflag=true;
+    }else if (temp=="N"||temp=="n") {
+      /*set 24 flag false*/
+      inputflag=true;
+      break;
+    }else{
+      std::cout << "Invalid input" << std::endl;
+      std::cout << "24 hr mode? (y/n)?" << std::endl;
+      inputflag=false;
+    }
+
+  }while(!(inputflag));
+  inputflag=false;
+
+  std::cout << "Set time:: (hh:mm:ss) \n" << std::endl;
+
+  std::cout << "HR: "
+  int hr;
+  std::cin >> hr;
+  std::cout << "\n MIN: "
+  int min;
+  std::cin >> min;
+  std::cout << "\n Sec: "
+  int sec;
+  std::cin >> sec;
+
+  clk.setHours(hr);
+  clk.setMinutes(min);
+  clk.setSeconds(sec);
+  
+  // std::cout << "Set time:: (hh:mm:ss) \n" << std::endl;
+  //
+
+  // std::string temptime;
+  //
+  // if ( temptime.size()!=8|| temptime.at(2)!=':' || temptime.at(5)!=':'){
+  //     std::cout << "Invalid input" << std::endl;
+  //   }else{
+  //
+  //
+  //   }
+  //
+  // }
+
+
+
+
+
+
+
+
+
 }
 
 
@@ -33,3 +101,4 @@ int main()
  *                                                                                                           return(elapsed.count());
  *                                                                                                           }
  *
+*/
