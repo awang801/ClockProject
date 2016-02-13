4d53603388678d0c3c06060a77920f781c724d84
James Ballard <j.lb4@live.com>
2016-02-12 18:27:44 -0600

    Reset function implatmented :)

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
 	timer* t=new timer();
 	Start(t);
 // user needs to have defined set the time before this point
-	std::cout <<"orignal time:" gettime(t);
+	std::cout <<"orignal time:"<< gettime(t);
 	std::cout <<"\n";
 
 	int startSecs = t->getSeconds();
@@ -116,12 +116,12 @@ void Start(timer* clk)
   std::cout << "MIN: \n";
   int min;
   std::cin >> min;
-	//min=min%60;
+	min=min%60;
 
   std::cout << "Sec:\n ";
   int sec;
   std::cin >> sec;
-	//sec=sec%60;
+	sec=sec%60;
 
 
 if(clk->get24hourmode()==false && am_pm=="pm")
@@ -167,44 +167,91 @@ std::string gettime(timer * clk)
  return t;
 }
 
-bool Reset(timer* clk)
-{
+
+
+
+bool Reset(timer* clk){
 		int i=1;
-		while(i==1)
-		{
+		while(i==1){
 			std::cout << "Clock menu \n" << "============ \n"<<"what do want to change(insert number) \n";
 			std::cout << "1) change clock mode " << std::endl;
 			std::cout << "2) change time" << std::endl;
 			std::cout << "3) end the clock" << std::endl;
-		}
-}
+			std::string am_pm;
 
-//
-// bool Reset(Timer* clk){
-// 		int i=1;
-// 		while(i==1){
-// 			std::cout << "Clock menu \n" << "============ \n"<<"what do want to change(insert number) \n";
-// 			std::cout << "1) change clock mode " << std::endl;
-// 			std::cout << "2) change time" << std::endl;
-// 			std::cout << "3) end the clock" << std::endl;
-//
-//
-// 			int input;
-// 			std::cin >> input;
-//
-//
-// 			if (input==1) {
-// 				clk->set24hourmode(!(clk->get24hourmode()));
-// 			}
-// 			else if (input==2) {
-// 				/* code */
-// 			}
-// 			else if(input==3) {
-// 				/* code */
-// 			}else{
-// 				std::cout << "invalid input" << std::endl;
-// 			}
-// 				std::cout << "would you like change anything else? (y/n)" << std::endl;
-// 					char x;
-//
-// 		}
+			int input;
+			std::cin >> input;
+
+
+			if (input==1) {
+				clk->set24hourmode(!(clk->get24hourmode()));
+
+
+			}
+			else if (input==2) {
+
+				if(!(clk->get24hourmode())){
+			      std::cout << "Am or PM?" << std::endl;
+
+			 			std::cin >> am_pm;
+			 			do {
+			 				if (am_pm=="am"||am_pm=="AM"||am_pm=="aM"||am_pm=="Am"||am_pm=="a.m.")
+			 				{
+
+			 					break;
+			 				} else if (am_pm=="pm"||am_pm=="PM"||am_pm=="pM"||am_pm=="Pm"||am_pm=="p.m.") {
+			 					am_pm=="pm"; //need am_pm to equal this later
+
+			 					break;
+			 				} else
+			 				{
+			 					std::cout << "Invalid input" << std::endl;
+			 					std::cout << "Am or PM?" << std::endl;
+			 					std::cin >> am_pm;
+			 				}
+			 			} while(true);
+					}
+					std::cout << "HR: \n";
+					int hr;
+					std::cin >> hr;
+					hr=hr%24;
+					std::cout << hr << std::endl;
+
+					std::cout << "MIN: \n";
+					int min;
+					std::cin >> min;
+					min=min%60;
+
+					std::cout << "Sec:\n ";
+					int sec;
+					std::cin >> sec;
+					sec=sec%60;
+
+					if(clk->get24hourmode()==false && am_pm=="pm")
+					{
+						hr=hr%12;
+						hr=hr+12;
+
+					}
+					  clk->setHours(hr);
+					  clk->setMinutes(min);
+					  clk->setSeconds(sec);
+
+
+			}
+			else if(input==3) {
+				return(false);
+			}else{
+				std::cout << "invalid input" << std::endl;
+			}
+				std::cout << "would you like change anything else? (y/n)" << std::endl;
+					char x;
+					std::cin >> x;
+				if (x=='n'||x=='N') {
+
+					//write the file here
+					return(true);
+				}
+		}
+		return (true);
+	}
