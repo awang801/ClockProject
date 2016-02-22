a585e189a610fb27e8569cbe4b72880c074570bc
John McCain <johnm.freestate@gmail.com>
2016-02-22 10:11:00 -0600

    Fix compilation on Windows by replacing std::to_string with custom function intToString Add .gitignore

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@
 #include "timer.h"
 #include <limits>
 
+//For int to string conversion
+#include <sstream>
+
 /*
 *	@pre a pointer to a timer is passed in
 *	@post the time of the clock is Set
@@ -30,6 +33,23 @@ bool Reset(timer* clk);
 */
 std::string gettime(timer* clk);
 
+
+
+/*
+*	Converts an integer to a std::string
+*	This is needed because std::to_string (as used in the original implementation) is not supported in every compiler 
+*	@param num the integer to be converted
+*	@returns a std::string representation of the given integer
+*/
+std::string intToStr(int num);
+
+std::string intToStr(int num){
+	std::ostringstream stringConverter;
+
+	stringConverter << num;
+
+	return (stringConverter.str());
+}
 
 int main()
 {
@@ -193,23 +213,23 @@ std::string gettime(timer * clk)
 
 	int temp=clk->getHours();
 	if (temp>=10) {
-		t=std::to_string(temp);
+		t=intToStr(temp);
 	}
 	else {
-		t="0"+std::to_string(temp);
+		t="0"+intToStr(temp);
 	}
 	temp=clk->getMinutes();
 	if (temp>=10) {
-		t=t+":"+std::to_string(temp);
+		t=t+":"+intToStr(temp);
 	}
 	else {
-		t=t+":0"+std::to_string(temp);
+		t=t+":0"+intToStr(temp);
 	}
 	temp=clk->getSeconds();
 	if (temp>=10) {
-		t=t+":"+std::to_string(temp);
+		t=t+":"+intToStr(temp);
 	}else {
-		t=t+":0"+std::to_string(temp);
+		t=t+":0"+intToStr(temp);
 	}
 	if(!(clk->get24hourmode())){
 		t=t+clk->getam_pm();
@@ -218,111 +238,114 @@ std::string gettime(timer * clk)
 }
 
 bool Reset(timer* clk){
-		int i=1;
-		while(i==1){
-			std::cout << "Clock menu \n" << "============ \n"<<"what do want to change(insert number) \n";
-			std::cout << "1) change clock mode " << std::endl;
-			std::cout << "2) change time" << std::endl;
-			std::cout << "3) end the clock" << std::endl;
-			std::string am_pm;
+	int i=1;
+	while(i==1){
+		std::cout << "Clock menu \n" << "============ \n"<<"what do want to change(insert number) \n";
+		std::cout << "1) change clock mode " << std::endl;
+		std::cout << "2) change time" << std::endl;
+		std::cout << "3) end the clock" << std::endl;
+		std::string am_pm;
 
-			int input;
-			std::cin >> input;
-
-
-			if (input==1) {
-				clk->set24hourmode(!(clk->get24hourmode()));
-			}
-			else if (input==2) {
-
-				if(!(clk->get24hourmode())){
-			      std::cout << "Am or PM?" << std::endl;
-
-			 			std::cin >> am_pm;
-			 			do {
-			 				if (am_pm=="am"||am_pm=="AM"||am_pm=="aM"||am_pm=="Am"||am_pm=="a.m.")
-			 				{
-
-			 					break;
-			 				} else if (am_pm=="pm"||am_pm=="PM"||am_pm=="pM"||am_pm=="Pm"||am_pm=="p.m.") {
-			 					am_pm=="pm"; //need am_pm to equal this later
-
-			 					break;
-			 				} else
-			 				{
-			 					std::cout << "Invalid input" << std::endl;
-
-			 					std::cout << "Am or PM?" << std::endl;
-			 					std::cin >> am_pm;
-			 				}
-			 			} while(true);
-					}
-					std::cout << "HR: \n";
-				  int hr;
-				  std::cin >> hr;
-					while(std::cin.fail()) {  //learned how to
-				    std::cin.clear();
-				    std::cin.ignore(std::numeric_limits<int>::max(),'\n');
-				    std::cout << "Bad entry.  Enter a NUMBER: ";
-				    std::cin >> hr;
-				}
-
-					hr=hr%24;
+		int input;
+		std::cin >> input;
 
 
-				  std::cout << "MIN: \n";
-				  int min;
-				  std::cin >> min;
-					while(std::cin.fail()) {
-				    std::cin.clear();
-				    std::cin.ignore(std::numeric_limits<int>::max(),'\n');
-				    std::cout << "Bad entry.  Enter a NUMBER: ";
-				    std::cin >> min;
-				}
-					min=min%60;
-
-				  std::cout << "Sec:\n ";
-				  int sec;
-				  std::cin >> sec;
-					while(std::cin.fail()) {
-				    std::cin.clear();
-				    std::cin.ignore(std::numeric_limits<int>::max(),'\n');
-				    std::cout << "Bad entry.  Enter a NUMBER: ";
-				    std::cin >> sec;
-				}
-					sec=sec%60;
-
-					if(clk->get24hourmode()==false)
-					{
-						if (am_pm=="pm") {
-							hr=hr%12;
-							hr=hr+12;
-						}
-						else if(hr==12) {
-							hr=0;
-						}
-
-
-					}
-					  clk->setHours(hr);
-					  clk->setMinutes(min);
-					  clk->setSeconds(sec);
-
-
-			}
-			else if(input==3) {
-				return(false);
-			}else{
-				std::cout << "invalid input" << std::endl;
-			}
-				std::cout << "would you like change anything else? (y/n)" << std::endl;
-					char x;
-					std::cin >> x;
-				if (x=='n'||x=='N') {
-
-					//write the file here
-					return(true);
-				}
+		if (input==1) {
+			clk->set24hourmode(!(clk->get24hourmode()));
 		}
-		return (true);
+		else if (input==2) {
+
+			if(!(clk->get24hourmode())){
+		      std::cout << "Am or PM?" << std::endl;
+
+		 			std::cin >> am_pm;
+		 			do {
+		 				if (am_pm=="am"||am_pm=="AM"||am_pm=="aM"||am_pm=="Am"||am_pm=="a.m.")
+		 				{
+
+		 					break;
+		 				} else if (am_pm=="pm"||am_pm=="PM"||am_pm=="pM"||am_pm=="Pm"||am_pm=="p.m.") {
+		 					am_pm=="pm"; //need am_pm to equal this later
+
+		 					break;
+		 				} else
+		 				{
+		 					std::cout << "Invalid input" << std::endl;
+
+		 					std::cout << "Am or PM?" << std::endl;
+		 					std::cin >> am_pm;
+		 				}
+		 			} while(true);
+				}
+				std::cout << "HR: \n";
+			  int hr;
+			  std::cin >> hr;
+				while(std::cin.fail()) {  //learned how to
+			    std::cin.clear();
+			    std::cin.ignore(std::numeric_limits<int>::max(),'\n');
+			    std::cout << "Bad entry.  Enter a NUMBER: ";
+			    std::cin >> hr;
+			}
+
+				hr=hr%24;
+
+
+			  std::cout << "MIN: \n";
+			  int min;
+			  std::cin >> min;
+				while(std::cin.fail()) {
+			    std::cin.clear();
+			    std::cin.ignore(std::numeric_limits<int>::max(),'\n');
+			    std::cout << "Bad entry.  Enter a NUMBER: ";
+			    std::cin >> min;
+			}
+				min=min%60;
+
+			  std::cout << "Sec:\n ";
+			  int sec;
+			  std::cin >> sec;
+				while(std::cin.fail()) {
+			    std::cin.clear();
+			    std::cin.ignore(std::numeric_limits<int>::max(),'\n');
+			    std::cout << "Bad entry.  Enter a NUMBER: ";
+			    std::cin >> sec;
+			}
+				sec=sec%60;
+
+				if(clk->get24hourmode()==false)
+				{
+					if (am_pm=="pm") {
+						hr=hr%12;
+						hr=hr+12;
+					}
+					else if(hr==12) {
+						hr=0;
+					}
+
+
+				}
+				  clk->setHours(hr);
+				  clk->setMinutes(min);
+				  clk->setSeconds(sec);
+
+
+		}
+		else if(input==3) {
+			return(false);
+		}else{
+			std::cout << "invalid input" << std::endl;
+		}
+			std::cout << "would you like change anything else? (y/n)" << std::endl;
+				char x;
+				std::cin >> x;
+			if (x=='n'||x=='N') {
+
+				//write the file here
+				return(true);
+			}
 	}
+	return (true);
+}
+
+
+
